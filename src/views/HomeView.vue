8468117161482a1467240071f212782167e252e8
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-05-11 13:37:00 +0300

    add main post section and bg main.jpg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,58 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <main>
+  <main class="main-post">
+    <div class="main-post__inner container">
+      <p class="main-post__vehicle">Vehicle</p>
+      <h1 class="main-post__title">One of Saturn’s largest rings may be newer than anyone</h1>
+      <div class="main-post__info">
+        <p class="main-post__date">June 6, 2019</p>
+        <p class="main-post__author"><span class="main-post__author--gray">By</span> Rickie Baroch</p>
+        <p class="main-post__coments">4 comments</p>
+      </div>
+    </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.main-post {
+  width: 100%;
+  height: 700px;
+  background: #000 url('@/assets/img/main.jpg') no-repeat center center;
+
+  &__inner {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-end;
+    color: #fff;
+    padding-bottom: 59px;
+  }
+
+  &__vehicle {
+    font-size: 14px;
+    line-height: 18px;
+    text-transform: uppercase;
+  }
+
+  &__title {
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 50px;
+    max-width: 560px;
+  }
+
+  &__info {
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+    line-height: 19px;
+    width: 283px;
+  }
+
+  &__author--gray {
+    color: #B4B4B4;
+  }
+}
+</style>
